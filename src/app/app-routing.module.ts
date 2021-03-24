8fb9c02944b66c29a4b0947f3a9c3451c6b404f4
Gosha <george.kovalenko.2005@gmail.com>
2021-03-24 17:49:45 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+//Component
+import { MainComponent } from './main/main.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { ArticleComponent } from './article/article.component';
+const routes: Routes = [
+  {path: "", component:MainComponent},
+  {path: "login", component: LoginComponent},
+  {path:"register", component: RegisterComponent},
+  {path:'article', component: ArticleListComponent},
+  {path:"add-article", component:AddArticleComponent},
+  {path: 'article/item/:id', component:ArticleComponent },
+  {path: "**", component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'app-angular';
+  activeUser = false;
+  ngDoCheck(){
+    if(localStorage.getItem('token')){
+      this.activeUser = true;
+    } else {
+      this.activeUser = false;
+    }
+  }
+  exitAccount(){
+    localStorage.removeItem('token');
+    location.reload();
+  }
 }
